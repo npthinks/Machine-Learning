1901277c630340e388eb1154b9a86ce5f81cdaaa
Nishanth Perumal <nishanthgovind2000@gmail.com>
2024-06-12 21:05:24 +0200

    Update and rename Code.py to code.py

--- code.py
+++ code.py
@@ -2,7 +2,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import statsmodels.api as sm
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
